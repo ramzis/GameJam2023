219f755d995ed11a19dde9d52c16689c523baddf
Tadas Audinis <tadas.audinis@vinted.com>
2023-02-04 06:42:36 +0200

    Add data objects

---
+++ Assets/Scripts/Data/ItemData.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item Data", menuName = "Data/Item", order = 1)]
+public class ItemData : ScriptableObject
+{
+    [Header("Metadata")]
+    public string title = "Item";
+    public Type type = Type.Ingredient;
+    public Category category = Category.Root;
+
+    [Header("Visuals")]
+    public Sprite available;
+    public Sprite extracted;
+    public Sprite empty;
+
+    public enum Type
+    {
+        Ingredient,
+        Trash
+    }
+
+    public enum Category
+    {
+        Root,
+        Sap,
+        Mushroom
+    }
+}
---
+++ Assets/Scripts/Data/RecipeData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Recipe Data", menuName = "Data/Recipe", order = 1)]
+public class RecipeData : ScriptableObject
+{
+    [Header("Metadata")]
+    public string title = "Recipe";
+
+    public List<ItemData> ingredients;
+}
