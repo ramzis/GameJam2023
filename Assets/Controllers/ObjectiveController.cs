da685752134a3ee3a21bddd7f7cc6cb1be847e39
Tadas Audinis <tadas.audinis@vinted.com>
2023-02-04 06:50:37 +0200

    Add outline methods

---
+++ Assets/Controllers/ObjectiveController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectiveController : MonoBehaviour
+{
+    public event Action OnObjectivesStarted;
+    public event Action OnObjectivesExhausted;
+
+    [SerializeField]
+    private List<RecipeData> recipes;
+
+    private RecipeData currentRecipe;
+
+    private void Start()
+    {
+        currentRecipe = recipes[0];
+        OnObjectivesStarted?.Invoke();
+    }
+
+    public bool EvaluateRecipe(HashSet<ItemData> ingredients)
+    {
+        return currentRecipe.ingredients.TrueForAll((x) =>
+            ingredients.Contains(x));
+    }
+}
---
+++ Assets/Managers/MainManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+[RequireComponent(typeof(RecipeController))]
+[RequireComponent(typeof(InventoryController))]
+[RequireComponent(typeof(ObjectiveController))]
+public class MainManager : MonoBehaviour
+{
+    // 1. Receive recipe
+    // 2. Collect to inventory
+    // 3. Give to witch
+
+    private RecipeController recipeController;
+    private InventoryController inventoryController;
+    private ObjectiveController objectiveController;
+
+    private void Awake()
+    {
+        ResolveControllers();
+        UnsubscribeEvents();
+        SubscribeEvents();
+    }
+
+    private void ResolveControllers()
+    {
+        recipeController = GetComponent<RecipeController>();
+        inventoryController = GetComponent<InventoryController>();
+        objectiveController = GetComponent<ObjectiveController>();
+    }
+
+    private void SubscribeEvents()
+    {
+        objectiveController.OnObjectivesStarted += OnObjectivesStartedHandler;
+        objectiveController.OnObjectivesExhausted += OnObjectivesExhaustedHandler;
+    }
+
+    private void UnsubscribeEvents()
+    {
+        objectiveController.OnObjectivesStarted -= OnObjectivesStartedHandler;
+        objectiveController.OnObjectivesExhausted -= OnObjectivesExhaustedHandler;
+    }
+
+    #region Objective Handler event handlers
+
+    private void OnObjectivesStartedHandler()
+    {
+        Debug.Log("OnObjectivesStartedHandler");
+    }
+
+    private void OnObjectivesExhaustedHandler()
+    {
+        Debug.Log("OnObjectivesExhaustedHandler");
+    }
+
+    #endregion
+}
