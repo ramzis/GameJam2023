d61a192de3dbb916ed503252b85f68087c8b874c
Tadas Audinis <tadas.audinis@vinted.com>
2023-02-04 13:37:30 +0200

    Update Objective Controller to use one recipe

--- Assets/Controllers/ObjectiveController.cs
+++ Assets/Controllers/ObjectiveController.cs
@@ -5,17 +5,19 @@ using System.Collections.Generic;
 public class ObjectiveController : MonoBehaviour
 {
     public event Action OnObjectivesStarted;
+    public event Action OnObjectiveProgressed;
     public event Action OnObjectivesExhausted;
-
-    [SerializeField]
-    private List<RecipeData> recipes;
 
     private RecipeData currentRecipe;
 
     private void Start()
     {
-        currentRecipe = recipes[0];
         OnObjectivesStarted?.Invoke();
+    }
+
+    public void SetCurrentRecipe(RecipeData recipe)
+    {
+        currentRecipe = recipe;
     }
 
     public bool EvaluateRecipe(HashSet<ItemData> ingredients)
