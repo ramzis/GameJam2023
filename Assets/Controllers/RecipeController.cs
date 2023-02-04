4b9235a505ca3253b6fa272cb4a81d0c1c8bea41
Tadas Audinis <tadas.audinis@vinted.com>
2023-02-04 12:30:49 +0200

    Add recipes

--- Assets/Controllers/RecipeController.cs
+++ Assets/Controllers/RecipeController.cs
@@ -1,5 +1,29 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class RecipeController : MonoBehaviour
 {
+    private int currentRecipeIndex;
+    [SerializeField]
+    private List<RecipeData> recipes;
+
+    private void Start()
+    {
+        if(recipes == null) recipes = new List<RecipeData>();
+        currentRecipeIndex = 0;
+    }
+
+    public RecipeData NextRecipe()
+    {
+        if (currentRecipeIndex >= recipes.Count) return null;
+
+        return recipes[currentRecipeIndex++];
+    }
+
+    public RecipeData CurrentRecipe()
+    {
+        if (currentRecipeIndex >= recipes.Count) return null;
+
+        return recipes[currentRecipeIndex];
+    }
 }
