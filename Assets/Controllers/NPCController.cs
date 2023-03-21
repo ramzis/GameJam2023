56528edd0e6a9e4c9fcf26f0a825005f4a2dd4a1
Tadas Audinis <tadas.audinis@vinted.com>
2023-03-21 22:55:10 +0200

    Update NPCController with events

--- Assets/Controllers/NPCController.cs
+++ Assets/Controllers/NPCController.cs
@@ -1,24 +1,44 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+using static ObjectiveController;
 
-public class NPCController : MonoBehaviour
+public class NPCController : MonoBehaviour, IListen<NPC>, IListen<ObjectiveController>
 {
     private List<NPC> npcs;
+
+    private EventListeners<NPCController> eventListeners;
+
+    #region Lifecycle Methods
 
     private void Awake()
     {
         npcs = new List<NPC>();
+        eventListeners = new EventListeners<NPCController>(this);
+    }
+
+    private void Start()
+    {
+        RegisterEvents();
     }
 
     private void Update()
     {
-        InteractRandomly();
+        //InteractRandomly();
     }
 
     private void OnDisable()
     {
         npcs.Clear();
     }
+
+    private void RegisterEvents()
+    {
+        eventListeners.RegisterEvent(new Event_Say());
+    }
+
+    #endregion
 
     private void InteractRandomly()
     {
@@ -31,20 +51,57 @@ public class NPCController : MonoBehaviour
         }
     }
 
-    public void Register(NPC npc)
+    public (List<int>, Action<dynamic>) HandleEvent(NPC component, IEvent<NPC> @event)
     {
-        npcs.Add(npc);
+        if(!npcs.Contains(component)) npcs.Add(component);
+
+        return @event switch
+        {
+            NPC.ClickedEvent e => (
+                new List<int>() { 0 },
+                (payload) =>
+                {
+                    payload.ReceiveMessage("Cycle mood");
+                }
+            ),
+            NPC.TextEvent e => (
+                new List<int>() { 0 },
+                (payload) =>
+                {
+                    //Debug.Log($"[NPCController]: ({component.name}) says: {payload}");
+                    eventListeners.RaiseEvent(new Event_Say(payload));
+                }
+            ),
+            _ => (null, null)
+        };
     }
 
-    public void Unregister(NPC npc)
+    public (List<int>, Action<dynamic>) HandleEvent(ObjectiveController component, IEvent<ObjectiveController> @event)
     {
-        Debug.Log($"[NPCController]: Unregistering {npc.name}");
-        npcs.Remove(npc);
+        return @event switch
+        {
+            Objective1Event e => (
+                new List<int>() { 0 },
+                (_) =>
+                {
+                    foreach (var npc in npcs) npc.ReceiveMessage("Get ready for Objective 1");
+                }
+            ),
+            _ => (null, null)
+        };
     }
 
-    public void NotifyEvent(NPC npc, string message)
+    public class Event_Say : IEvent<NPCController>
     {
-        Debug.Log($"[NPCController]: ({npc.name}) says: {message}");
-        npc.ReceiveMessage("GOTCHA!");
+        private readonly string message;
+
+        public Event_Say(string message = "")
+        {
+            this.message = message;
+        }
+
+        public string GetName() => "NPCControllerSayEvent";
+
+        public dynamic GetPayload() => message;
     }
 }
