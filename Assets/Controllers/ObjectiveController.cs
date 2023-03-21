72f986ed38d8ae945e7f423ca3040a25aba2ec74
Tadas Audinis <tadas.audinis@vinted.com>
2023-03-21 22:55:22 +0200

    Update ObjectiveController with events

--- Assets/Controllers/ObjectiveController.cs
+++ Assets/Controllers/ObjectiveController.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections.Generic;
-using System.Collections;
-using UnityEngine.SceneManagement;
 
 public class ObjectiveController : MonoBehaviour
 {
+    protected EventListeners<ObjectiveController> eventListeners;
+
     public event Action OnRequestNewRecipe;
     public event Action OnRequestPoisonRecipe;
     public event Action OnRequestNewCategories;
@@ -26,9 +26,16 @@ public class ObjectiveController : MonoBehaviour
     private bool poisonProvided;
     private bool firstValidIngredientFound;
 
+
+    private void Awake()
+    {
+        eventListeners = new EventListeners<ObjectiveController>(this);
+    }
+
     private void Start()
     {
-        StartCoroutine(GameLoop());
+        eventListeners.RegisterEvent(new Objective1Event());
+        //StartCoroutine(GameLoop());
     }
 
     public void SetCurrentRecipe(RecipeData recipe)
@@ -72,98 +79,113 @@ public class ObjectiveController : MonoBehaviour
         if (firstValidIngredientFound) return;
 
         firstValidIngredientFound = true;
-        OnRequestFirstIngredientDialog?.Invoke(level-1);
+        OnRequestFirstIngredientDialog?.Invoke(level - 1);
     }
 
-    private IEnumerator GameLoop()
+    //private IEnumerator GameLoop()
+    //{
+    //    Debug.Log("[GameLoop]: Requesting poison recipe");
+    //    OnRequestPoisonRecipe?.Invoke();
+
+    //    while (true)
+    //    {
+    //        Debug.Log("[GameLoop]: Requesting recipe");
+    //        OnRequestNewRecipe?.Invoke();
+    //        OnRequestIntroDialog?.Invoke(level-1);
+
+    //        lastRecipeValid = false;
+    //        potionProvided = false;
+    //        poisonProvided = false;
+    //        firstValidIngredientFound = false;
+
+    //        yield return new WaitUntil(() => potionProvided);
+
+    //        Debug.Log("[GameLoop]: Evaluating recipe");
+
+    //        // All levels
+    //        if (level < 5)
+    //        {
+    //            if (lastRecipeValid)
+    //            {
+    //                Debug.Log("OnRequestCorrectIngredientDialog Invoke");
+    //                OnRequestCorrectIngredientDialog?.Invoke(level-1);
+    //                yield return SleepCutscene();
+    //            }
+
+    //            else
+    //            {
+    //                OnRequestWrongIngredientDialog?.Invoke(level-1);
+    //                --lives;
+    //                OnLivesCountChanged?.Invoke(lives);
+    //                if (lives < 1)
+    //                {
+
+    //                    StartCoroutine(EarlyGameOver());
+    //                    yield break;
+    //                }
+    //            }
+    //        }
+    //        // Last level
+    //        else
+    //        {
+    //            if(poisonProvided)
+    //            {
+    //                StartCoroutine(PoisonCutscene());
+    //                yield break;
+    //            }
+
+    //            lives = 0;
+    //            OnLivesCountChanged?.Invoke(lives);
+
+    //            // You fail even if the recipe is "valid"
+    //            StartCoroutine(GameOver());
+    //            yield break;
+    //        }
+
+    //        OnRequestNewCategories?.Invoke();
+    //        level++;
+    //    }
+    //}
+
+    //private IEnumerator PoisonCutscene()
+    //{
+    //    Debug.Log("[GameLoop]: You poisoned the witch!");
+    //    SceneManager.LoadScene("VictoryScreen");
+    //    yield return null;
+    //}
+
+    //private IEnumerator SleepCutscene()
+    //{
+    //    Debug.Log("[GameLoop]: The witch fell asleep...");
+    //    yield return null;
+    //}
+
+    //private IEnumerator EarlyGameOver()
+    //{
+    //    Debug.Log("[GameLoop]: The witch got angry and ate you!");
+    //    SceneManager.LoadScene("DefeatScreen");
+    //    yield return null;
+    //}
+
+    //private IEnumerator GameOver()
+    //{
+    //    Debug.Log("[GameLoop]: You did well, but the witch still ate you!");
+    //    SceneManager.LoadScene("DefeatScreen");
+    //    yield return null;
+    //}
+
+    private bool started;
+    private void Update()
     {
-        Debug.Log("[GameLoop]: Requesting poison recipe");
-        OnRequestPoisonRecipe?.Invoke();
-
-        while (true)
-        {
-            Debug.Log("[GameLoop]: Requesting recipe");
-            OnRequestNewRecipe?.Invoke();
-            OnRequestIntroDialog?.Invoke(level-1);
-
-            lastRecipeValid = false;
-            potionProvided = false;
-            poisonProvided = false;
-            firstValidIngredientFound = false;
-
-            yield return new WaitUntil(() => potionProvided);
-
-            Debug.Log("[GameLoop]: Evaluating recipe");
-
-            // All levels
-            if (level < 5)
-            {
-                if (lastRecipeValid)
-                {
-                    Debug.Log("OnRequestCorrectIngredientDialog Invoke");
-                    OnRequestCorrectIngredientDialog?.Invoke(level-1);
-                    yield return SleepCutscene();
-                }
-
-                else
-                {
-                    OnRequestWrongIngredientDialog?.Invoke(level-1);
-                    --lives;
-                    OnLivesCountChanged?.Invoke(lives);
-                    if (lives < 1)
-                    {
-                        
-                        StartCoroutine(EarlyGameOver());
-                        yield break;
-                    }
-                }
-            }
-            // Last level
-            else
-            {
-                if(poisonProvided)
-                {
-                    StartCoroutine(PoisonCutscene());
-                    yield break;
-                }
-
-                lives = 0;
-                OnLivesCountChanged?.Invoke(lives);
-
-                // You fail even if the recipe is "valid"
-                StartCoroutine(GameOver());
-                yield break;
-            }
-
-            OnRequestNewCategories?.Invoke();
-            level++;
-        }
+        if (started && !Input.GetKeyDown(KeyCode.O)) return;
+        eventListeners.RaiseEvent(new Objective1Event());
+        started = true;
     }
 
-    private IEnumerator PoisonCutscene()
+    public class Objective1Event : IEvent<ObjectiveController>
     {
-        Debug.Log("[GameLoop]: You poisoned the witch!");
-        SceneManager.LoadScene("VictoryScreen");
-        yield return null;
-    }
+        public string GetName() => "Objective1Event";
 
-    private IEnumerator SleepCutscene()
-    {
-        Debug.Log("[GameLoop]: The witch fell asleep...");
-        yield return null;
-    }
-
-    private IEnumerator EarlyGameOver()
-    {
-        Debug.Log("[GameLoop]: The witch got angry and ate you!");
-        SceneManager.LoadScene("DefeatScreen");
-        yield return null;
-    }
-
-    private IEnumerator GameOver()
-    {
-        Debug.Log("[GameLoop]: You did well, but the witch still ate you!");
-        SceneManager.LoadScene("DefeatScreen");
-        yield return null;
+        public dynamic GetPayload() => null;
     }
 }
